77dbdd2ee780966a9dea9cd931aee61edd386404
Maria Öhrman <maria.ohrman@gu.se>
2024-11-22 12:45:01 +0100

    chore: major cleanup

--- strixconfigurer/corpusconf.py
+++ strixconfigurer/corpusconf.py
@@ -1,4 +1,3 @@
-# corpusconfig
 import json
 import os
 import glob
@@ -68,7 +67,6 @@ class CorpusConfig:
         text_attributes_list = {}
         for key, conf in self._all_config_files.items():
             try:
-                # text_attributes[key] = dict((attr, self._text_attributes[attr]) for attr in conf["analyze_config"]["text_attributes"])
                 tempDict = {}
                 for attr_dict in conf["analyze_config"]["text_attributes"]:
                     for attr_name, attr in attr_dict.items():
@@ -83,6 +81,7 @@ class CorpusConfig:
                 continue
             if "title" in text_attributes[key]:
                 del text_attributes[key]["title"]
+        # TODO fix hardcoded
         text_attributes_list['yearR'] = {'name': 'yearR', 'translation_name': {'swe': 'År', 'eng': 'Year'}}
         # TODO WHY do we need this???
         text_attributes["litteraturbanken"] = text_attributes_list
@@ -103,17 +102,9 @@ class CorpusConfig:
     def get_struct_elem(self, elem):
         return self._struct_elems[elem]
 
-    # def _load_type_info(self):
-    #     type_file = os.path.join(self.settings_dir, "attributes/types.json")
-    #     return json.load(open(type_file))
-
     def _load_type_info(self):
          with open(os.path.join(self.settings_dir, "attributes/types.yaml")) as file:
             return yaml.load(file, Loader=SafeLoader)
-
-    # def _load_struct_elems(self):
-    #     type_file = os.path.join(self.settings_dir, "attributes/struct_elems.json")
-    #     return json.load(open(type_file))
 
     def _load_struct_elems(self):
         with open(os.path.join(self.settings_dir, "attributes/struct_elems.yaml")) as file:
@@ -135,33 +126,21 @@ class CorpusConfig:
 
     def _fetch_corpus_conf(self, corpus_id, config_type="corpora"):
         """
-        Open requested corpus settings file and recursively fetch and merge
-        with parent config file, if there is one.
+        Open requested corpus settings file
         :param corpus_id: id of corpus to fetch
         :return: a dict containing configuration for corpus
         """
         config_file = self._get_config_file(corpus_id, config_type)
         try:
-            # config_obj = json.load(open(config_file))
             config_obj = yaml.load(open(config_file), Loader=SafeLoader)
         except Exception:
             self.logger.error("Could not read config file: " + config_file)
             raise
 
-        parents = config_obj.get("parents", [])
-        for parent in parents:
-            parent_obj = self._fetch_corpus_conf(parent, config_type="corpora_templates")
-            _merge_configs(config_obj, parent_obj)
         return config_obj
 
-    # def _get_config_file(self, corpus_id, config_type="corpora"):
-    #     return os.path.join(self.settings_dir, config_type, corpus_id + ".json")
-    
     def _get_config_file(self, corpus_id, config_type="corpora"):
         return os.path.join(self.settings_dir, config_type, corpus_id + ".yaml") 
-
-    # def _get_attributes(self, attr_type):
-    #     return json.load(open(os.path.join(self.settings_dir, "attributes", attr_type + ".json")))
 
     def _get_attributes(self, attr_type):
         with open(os.path.join(self.settings_dir, "attributes", attr_type + ".yaml")) as file:
@@ -171,30 +150,3 @@ class CorpusConfig:
         with open(os.path.join(self.settings_dir, "all_folders.yaml")) as file:
             return yaml.load(file, Loader=SafeLoader)
 
-def _merge_configs(target, source):
-    """
-    Merge two corpus configurations.
-    Moves attributes from source to target, so any definitions in both will
-    be overwritten by source.
-    :return: A new corpus configuration.
-    """
-    for k, v in source.items():
-        if k in target:
-            if k == "analyze_config":
-                for k2, v2 in source["analyze_config"].items():
-                    if k2 in ["text_attributes", "word_attributes"]:
-                        if k2 not in target["analyze_config"]:
-                            target["analyze_config"][k2] = []
-                        v2.extend(target["analyze_config"][k2])
-                        target["analyze_config"][k2] = v2
-                    elif k2 == "struct_attributes":
-                        for k3, v3 in source["analyze_config"]["struct_attributes"].items():
-                            if "struct_attributes" not in target["analyze_config"]:
-                                target["analyze_config"]["struct_attributes"] = {}
-                            elif k3 in target["analyze_config"]["struct_attributes"]:
-                                v3.extend(target["analyze_config"]["struct_attributes"][k3])
-                            target["analyze_config"]["struct_attributes"][k3] = v3
-                    else:
-                        raise ValueError("Key: " + k + "." + k2 + ", not allowed in parent configuration.")
-        else:
-            target[k] = v
