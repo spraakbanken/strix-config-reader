e06f5e71df6af93af3743ff113dc1c96426f78df
Maria Öhrman <maria.ohrman@gu.se>
2024-11-25 10:02:23 +0100

    fixes and refactorings

--- strixconfigurer/corpusconf.py
+++ strixconfigurer/corpusconf.py
@@ -1,8 +1,6 @@
-import json
 import os
 import glob
 import logging
-import importlib.machinery
 
 import yaml
 from yaml.loader import SafeLoader
@@ -13,14 +11,16 @@ class CorpusConfig:
         self.logger = logging.getLogger(__name__)
         self.settings_dir = settings_dir
         self._all_config_files = self._get_all_config_files()
-        self.corpora_protected = self._get_protected()
-        self._word_attributes = self._get_attributes("word_attributes")
-        self._struct_attributes = self._get_attributes("struct_attributes")
-        self._text_attributes = self._get_attributes("text_attributes")
         self._type_info = self._load_type_info()
         self._struct_elems = self._load_struct_elems()
+        self._modes = self._load_modes()
+        self._word_attributes = self._load_attributes("positional")
+        self._struct_attributes = self._load_attributes("structural")
 
     def get_modes(self):
+        return self._modes
+
+    def _load_modes(self):
         modes = {}
         for filename in glob.glob(os.path.join(self.settings_dir, "modes/*")):
             with open(filename) as file:
@@ -40,75 +40,69 @@ class CorpusConfig:
         """
         return corpus_id in self._all_config_files
 
+
     def get_word_attribute(self, attr_name):
         return self._word_attributes[attr_name]
-
-    def get_word_attributeX(self, attr_name):
-        with open(os.path.join(self.settings_dir, "attributes/positional/"+attr_name+".yaml")) as file:
-            return yaml.load(file, Loader=SafeLoader)
-
-    def get_word_attributes(self):
-        return self._word_attributes
 
     def get_struct_attribute(self, attr_name):
         return self._struct_attributes[attr_name]
 
-    def get_struct_attributeX(self, attr_name):
-        with open(os.path.join(self.settings_dir, "attributes/structural/"+attr_name+".yaml")) as file:
-            return yaml.load(file, Loader=SafeLoader)
+    def _load_attributes(self, type):
+        attrs = {}
+        for path in glob.glob(os.path.join(self.settings_dir, f"attributes/{type}/*.yaml")):
+            with open(path) as file:
+                attr = yaml.load(file, Loader=SafeLoader)
+                attr_name = os.path.splitext(os.path.basename(path))[0]
+                attrs[attr_name] = attr
+        return attrs
 
-    def get_struct_attributes(self):
-        return self._struct_attributes
+    # TODO fix name
+    def get_text_attributes_list(self):
+        text_attributes_list = {}
+        for key, conf in self._all_config_files.items():
+            for attr_dict in conf["analyze_config"].get("text_attributes", []):
+                for attr_name, attr in attr_dict.items():
+                    if type(attr) is str:
+                        attr = self.get_struct_attribute(attr)
+                    # TODO  this overwrites previous instances of the value and does not check for  mismatches
+                    text_attributes_list[attr_name] = attr
+        text_attributes_list['yearR'] = {'name': 'yearR', 'translation_name': {'swe': 'År', 'eng': 'Year'}}
+        return text_attributes_list
 
-    def get_text_attribute(self, attr_name):
-        return self._text_attributes[attr_name]
-
-    def get_text_attributeX(self, attr_name):
-        with open(os.path.join(self.settings_dir, "attributes/structural/"+attr_name+".yaml")) as file:
-            return yaml.load(file, Loader=SafeLoader)
 
     def get_text_attributes_by_corpora(self):
         """
         :return: a dict containing all text attributes by corpora
         """
         text_attributes = {}
-        text_attributes_list = {}
         for key, conf in self._all_config_files.items():
             try:
-                tempDict = {}
+                attrs = {}
                 for attr_dict in conf["analyze_config"]["text_attributes"]:
                     for attr_name, attr in attr_dict.items():
                         if type(attr) is str:
-                            attr = self.get_text_attributeX(attr)
-                        tempDict[attr_name] = attr
-                        text_attributes_list[attr_name] = attr
-                text_attributes[key] = tempDict
-                        
+                            attr = self.get_struct_attribute(attr)
+                        attrs[attr_name] = attr
+                text_attributes[key] = attrs
+
             except KeyError:
                 self.logger.info("No text attributes for corpus: %s" % key)
                 continue
             if "title" in text_attributes[key]:
                 del text_attributes[key]["title"]
-        # TODO fix hardcoded
-        text_attributes_list['yearR'] = {'name': 'yearR', 'translation_name': {'swe': 'År', 'eng': 'Year'}}
-        # TODO WHY do we need this???
-        text_attributes["litteraturbanken"] = text_attributes_list
         return text_attributes
-
-    def get_text_attributes(self):
-        return self._text_attributes
-
-    def is_ranked(self, word_attribute):
-        try:
-            return self._word_attributes[word_attribute].get("ranked", False)
-        except KeyError:
-            raise ValueError("\"" + word_attribute + "\" is not configured")
 
     def get_type_info(self):
         return self._type_info
 
     def get_struct_elem(self, elem):
         return self._struct_elems[elem]
+
+    def get_protected(self):
+        config_protected = {}
+        for key, conf in self._all_config_files.items():
+            config_protected[key] = conf.get("protected", False)
+        return config_protected
 
     def _load_type_info(self):
          with open(os.path.join(self.settings_dir, "attributes/types.yaml")) as file:
@@ -124,21 +118,14 @@ class CorpusConfig:
             key = os.path.splitext(os.path.basename(file))[0]
             config_files[key] = self._fetch_corpus_conf(key)
         return config_files
-    
-    def _get_protected(self):
-        config_protected = {}
-        for file in glob.glob(self._get_config_file("*")):
-            key = os.path.splitext(os.path.basename(file))[0]
-            config_protected[key] = self._fetch_corpus_conf(key).get("protected", False)
-        return config_protected
 
-    def _fetch_corpus_conf(self, corpus_id, config_type="corpora"):
+    def _fetch_corpus_conf(self, corpus_id):
         """
         Open requested corpus settings file
         :param corpus_id: id of corpus to fetch
         :return: a dict containing configuration for corpus
         """
-        config_file = self._get_config_file(corpus_id, config_type)
+        config_file = self._get_config_file(corpus_id)
         try:
             config_obj = yaml.load(open(config_file), Loader=SafeLoader)
         except Exception:
@@ -147,14 +134,5 @@ class CorpusConfig:
 
         return config_obj
 
-    def _get_config_file(self, corpus_id, config_type="corpora"):
-        return os.path.join(self.settings_dir, config_type, corpus_id + ".yaml") 
-
-    def _get_attributes(self, attr_type):
-        with open(os.path.join(self.settings_dir, "attributes", attr_type + ".yaml")) as file:
-            return yaml.load(file, Loader=SafeLoader)
-
-    def get_folders_info(self):
-        with open(os.path.join(self.settings_dir, "all_folders.yaml")) as file:
-            return yaml.load(file, Loader=SafeLoader)
-
+    def _get_config_file(self, corpus_id):
+        return os.path.join(self.settings_dir, "corpora", corpus_id + ".yaml")
